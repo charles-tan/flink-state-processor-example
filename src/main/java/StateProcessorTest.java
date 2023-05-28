2018bde5713d119feeb878b2527d84754d10cc82
Till Rohrmann <trohrmann@apache.org>
2023-05-28 15:07:49 +0200

    More idiomatic Kafka savepoint reading

--- src/main/java/StateProcessorTest.java
+++ src/main/java/StateProcessorTest.java
@@ -2,11 +2,14 @@ import java.util.Arrays;
 import org.apache.flink.api.common.typeinfo.PrimitiveArrayTypeInfo;
 import org.apache.flink.connector.kafka.source.split.KafkaPartitionSplit;
 import org.apache.flink.connector.kafka.source.split.KafkaPartitionSplitSerializer;
+import org.apache.flink.core.io.SimpleVersionedSerializer;
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.state.api.OperatorIdentifier;
 import org.apache.flink.state.api.SavepointReader;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.TestUtils;
+import org.apache.flink.streaming.api.operators.util.SimpleVersionedListState;
 import org.apache.flink.util.CloseableIterator;
 
 public class StateProcessorTest {
@@ -22,13 +25,11 @@ public class StateProcessorTest {
             "SourceReaderState",
             PrimitiveArrayTypeInfo.BYTE_PRIMITIVE_ARRAY_TYPE_INFO);
         CloseableIterator<byte[]> a = listState.executeAndCollect();
-        while (a.hasNext()) {
-            byte[] aa = a.next();
-            System.out.println(aa);
-            KafkaPartitionSplitSerializer serializer = new KafkaPartitionSplitSerializer();
-            KafkaPartitionSplit split = serializer.deserialize(serializer.getVersion(), Arrays.copyOfRange(aa, 8, aa.length));
-            System.out.println(
-                String.format("topic=%s, partition=%s, startingOffset=%s, stoppingOffset=%s, topicPartition=%s",
+        TestUtils.MockListState<byte[]> rawBytes = new TestUtils.MockListState<>();
+        a.forEachRemaining(rawBytes::add);
+        SimpleVersionedListState<KafkaPartitionSplit> partitionOffsets = new SimpleVersionedListState<>(rawBytes, new KafkaPartitionSplitSerializer());
+        for (KafkaPartitionSplit split : partitionOffsets.get()) {
+            System.out.println(String.format("topic=%s, partition=%s, startingOffset=%s, stoppingOffset=%s, topicPartition=%s",
                     split.getTopic(), split.getPartition(),
                     split.getStartingOffset(), split.getStoppingOffset(), split.getTopicPartition()));
         }
